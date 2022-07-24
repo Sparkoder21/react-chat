0ce62a062492b4acbb3ea1c8bc336171c9fcb9ca
Anuj Chowdhury <109861258+Sparkoder21@users.noreply.github.com>
2022-07-24 19:36:47 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import {Box, Typography, Icon} from "@material-ui/core";
 function Footer(props){
     return (
         <Box position="fixed" left="0" bottom="0" width="100%">
-            <Typography style={{backgroundColor: props.theme.palette.primary.main, userSelect: "none"}} variant="subtitle2" color="textSecondary" align="center"><Icon className="material-icons" fontSize="small"> copyright </Icon> 2022 QuickCoder. All Rights Reserved. </Typography>
+            <Typography style={{backgroundColor: props.theme.palette.primary.main, userSelect: "none"}} variant="subtitle2" color="textSecondary" align="center"><Icon className="material-icons" fontSize="small"> copyright </Icon> 2022 Sparkoder. All Rights Reserved. </Typography>
         </Box>
     );
 }
